7510055cd9448e2b602aae291a329520546e7acc
Adrian <59284453+adrianrabei@users.noreply.github.com>
2020-09-02 18:18:19 +0300

    Cube color changer, cannon bullet instantiation

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    public GameObject ball_prefab;
+    private GameObject ball;
+    public Vector3 spawn_position;
+    private float t;
+    private Transform startPosx;
+    private Transform finishPosx;
+    private bool clicked;
+
+    void Start()
+    {
+        t = 0.01f;
+        clicked = false;
+    }
+
+    private void Shoot()
+    {
+        spawn_position = new Vector3(transform.position.x - 1.5f, transform.position.y, transform.position.z);
+        ball = Instantiate(ball_prefab, spawn_position, Quaternion.identity) as GameObject;
+        ball.transform.tag = gameObject.transform.tag;
+        startPosx = ball.transform.Find("start_pos");
+        finishPosx = ball.transform.Find("finish_pos");
+    }
+
+    private void OnMouseDown()
+    {
+        clicked = true;
+        Shoot();
+    }
+
+    void FixedUpdate()
+    {
+        if(clicked)
+        {
+            ball.transform.position = Vector3.Lerp(startPosx.transform.transform.position, finishPosx.transform.position, t);
+            t += 0.05f;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material m_red;
+    public Material m_blue;
+    public Material m_green;
+    public Material m_yellow;
+    private Material obj_mat;
+
+    void Start()
+    {
+        obj_mat = GetComponent<Renderer>().material;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ChangeColor(string color_to_change)
+    {
+        if(color_to_change == "Red")
+        {
+            obj_mat.color = m_red.color;
+        }
+        else if(color_to_change == "Blue")
+        {
+            obj_mat.color = m_blue.color;
+        }
+        else if(color_to_change == "Green")
+        {
+            obj_mat.color = m_green.color;
+        }
+        else if(color_to_change == "Yellow")
+        {
+            obj_mat.color = m_yellow.color;
+        }
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "RedBall")
+        {
+            ChangeColor("Red");
+        }
+        else if(other.gameObject.transform.tag == "BlueBall")
+        {
+            ChangeColor("Blue");
+        }
+        else if(other.gameObject.transform.tag == "GreenBall")
+        {
+            ChangeColor("Green");
+        }
+        else 
+        {
+            ChangeColor("Yellow");
+        }
+    }
+}
